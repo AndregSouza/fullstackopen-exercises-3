b87fa79135335b2895bb07d69d2602e3acfcfd7a
AndregSouza <andregsouzaweb@gmail.com>
2024-02-25 13:42:28 -0300

    Revert "test"
    This reverts commit 293fc153b4ce123dcb8bc1da03ce6b91798cd502.

--- part3/index.js
+++ part3/index.js
@@ -2,72 +2,60 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
     }
 ]
 
 app.get('/', (request, response) => {
-    console.log(persons);
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-    
-    response.json(persons)
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    const persons = persons.find(persons => persons.id === id)
+    const note = notes.find(note => note.id === id)
 
-    if (persons) {
-        response.json(persons)
+    if (note) {
+        response.json(note)
     } else {
         response.status(404).end()
     }
 })
 
-app.get('/info', (request, response) => {
-    const date = Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people </p> <p>${date}</p>`)
-})
-
-app.post('/api/persons', (request, response) => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id)) 
+app.post('/api/notes', (request, response) => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id)) 
       : 0
   
-    const persons = request.body
-    persons.id = maxId + 1
+    const note = request.body
+    note.id = maxId + 1
   
-    persons = persons.concat(persons)
+    notes = notes.concat(note)
   
-    response.json(persons)
+    response.json(note)
   })
 
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    persons = persons.filter(persons => persons.id !== id)
+    notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
 })
