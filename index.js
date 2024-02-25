40f0664397c0b91e2df33266151d10152e322003
AndregSouza <andregsouzaweb@gmail.com>
2024-02-25 19:07:29 -0300

    test

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
     },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
     },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
     },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
     }
 ]
 
@@ -51,28 +51,17 @@ app.get('/info', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-
-    const person = request.body
-
-    const isNameIncluded = persons.some(object => object.name === person.name);
-
-    if (person.name === "" || isNameIncluded == true || person.number === "") {
-        response.status(400).send({ error: 'names must be unique' })
-    }
-
-    console.log(duplicates, person.name);
-
     const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0
+      ? Math.max(...persons.map(n => n.id)) 
+      : 0
 
+    const persons = request.body
+    persons.id = maxId + 1
 
-    person.id = maxId + 1
-
-    persons = persons.concat(person)
+    persons = persons.concat(persons)
 
     response.json(persons)
-})
+  })
 
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -84,5 +73,5 @@ app.delete('/api/persons/:id', (request, response) => {
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
